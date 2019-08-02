af586e3f97fe46de535cd8e182c2fc39275011b6
fateyan <fateyan.tw@gmail.com>
2019-08-02 22:01:46 +0000

    orz

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const exec = require('child_process').exec;
+const exec = require('child_process').execSync;
 const fs   = require('fs');
 const crypto = require('crypto');
 const dbg = (s) => {
@@ -45,14 +45,16 @@ const sig = sign.sign(privateKey, 'hex');
 const deployNew = (prev) => {
     // deploy a new version
     const contract_name = "contract/firmware.liq";
-    if (!prev) prev = params.prevVer;
+    if (!prev) prev = params.prevAddr;
     const payload =
-         `tezos-client -A ${tezos_addr}` +
-         `for ${params.account}` +
-         `tranferring ${params.amount}` +
-         `from ${params.account}` +
-         `running ${contract_name}` +
-         `--init '(Pair "${params.account_addr}" (Pair "0.0.0" (Pair "${prev}" (Pair "${params.url}" "${sig}"))))'`
+         `tezos-client -A ${tezos_addr} ` +
+	 `originate contract ${params.firmware_name} ` +
+         `for ${params.account} ` +
+         `transferring ${params.amount} ` +
+         `from ${params.account} ` +
+         `running ${contract_name} ` +
+         `--init '(Pair "${params.account_addr}" (Pair "${params.version}" (Pair "${prev}" (Pair "${params.url}/${params.firmware_name}_${params.version}/${params.firmware}" "${sig}"))))'`
+	console.log(payload);
     exec(payload);
 };
 
@@ -60,11 +62,12 @@ const deployNew = (prev) => {
 if (fs.existsSync(server_folder + '/' +  params.firmware_name)) {
     const root_contract_name = "contract/firmware_root.liq";
     const payload =
-         `tezos-client -A ${tezos_addr}` +
-         `for ${params.account}` +
-         `tranferring ${params.amount}` +
-         `from ${params.account}` +
-         `running ${root_contract_name}` +
+         `tezos-client -A ${tezos_addr} ` +
+	 `originate contract ${params.firmware_name} ` +
+         `for ${params.account} ` +
+         `transferring ${params.amount} ` +
+         `from ${params.account} ` +
+         `running ${root_contract_name} ` +
          `--init '(Pair "${params.account_addr}" (Pair "0.0.0" (Pair "" (Pair "${params.firmware_name}" "${sig}"))))'`;
     exec(payload, () => {
         deployNew();
@@ -75,4 +78,5 @@ if (fs.existsSync(server_folder + '/' +  params.firmware_name)) {
 
 
 // 2. move firmware files to file server folder.
-exec(`mv ${params.firmware} ${server_folder}/${params.firmware_name}/`);
+exec(`mv ${params.firmware} ${server_folder}/${params.firmware_name}_${params.version}/`);
+
