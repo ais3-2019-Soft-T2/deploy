1c2828c84fe6b056d75157d278f46534ec3b60e7
fateyan <fateyan.tw@gmail.com>
2019-08-03 01:09:23 +0800

    fix the control flow

--- deploy.js
+++ deploy.js
@@ -18,12 +18,6 @@ const raw = fs.readFileSync(payload);
 const params = JSON.parse(payload);
 
 // 1. deploy contract
-let payload =
-     `tezos-client -A ${tezos_addr}` +
-     `for ${params.account}` +
-     `tranferring ${params.amount}` +
-     `from ${params.account}`;
-
 // generate digital signature
 const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
       namedCurve: 'sect239k1'
@@ -32,22 +26,37 @@ const sign = crypto.createSign('SHA256');
 sign.write(fs.readFileSync(params.firmware)).end();
 const sig = sign.sign(privateKey, 'hex');
 
-// if it's the first version of the firmware. (check by firmware_name, I think it's a unique identity)
-if (fs.existsSync(server_folder + params.firmware_name)) {
-    // deploy a new root contract
-    const root_contract_name = "contract/firmware_root.liq";
-    payload +=
-         `running ${root_contract_name}` +
-         `--init '(${params.account_addr}, "0.0.0", "", ${params.firmware_name}, ${sig})'`
-} else {
+const deployNew = (prev) => {
     // deploy a new version
     const contract_name = "contract/firmware.liq";
-    payload +=
+    if (!prev) prev = params.prevVer;
+    const payload =
+         `tezos-client -A ${tezos_addr}` +
+         `for ${params.account}` +
+         `tranferring ${params.amount}` +
+         `from ${params.account}` +
          `running ${contract_name}` +
-         `--init '(${params.account_addr}, "0.0.0", ${params.prevVer}, ${params.url}, ${sig})'`
+         `--init '(${params.account_addr}, "0.0.0", ${prev}, ${params.url}, ${sig})'`
+    exec(payload);
+};
+
+// if it's the first version of the firmware. (check by firmware_name, I think it's a unique identity)
+if (fs.existsSync(server_folder + params.firmware_name)) {
+    const root_contract_name = "contract/firmware_root.liq";
+    const payload =
+         `tezos-client -A ${tezos_addr}` +
+         `for ${params.account}` +
+         `tranferring ${params.amount}` +
+         `from ${params.account}` +
+         `running ${root_contract_name}` +
+         `--init '(${params.account_addr}, "0.0.0", "", ${params.firmware_name}, ${sig})'`;
+    exec(payload, () => {
+        deployNew();
+    });
+} else {
+    deployNew();
 }
 
-exec(payload);
 
 // 2. move firmware files to file server folder.
-exec(`mv ${params.firmware} ${server_folder}/${params.firmware_name/}`);
+exec(`mv ${params.firmware} ${server_folder}/${params.firmware_name}/`);
